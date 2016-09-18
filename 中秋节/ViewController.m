0858216d6bee8ca79eae26516c12a0c27550d323
feifanIt <feifanit@yolandaliuyao.lan>
2016-09-18 10:06:50 +0800

    已修改

--- 中秋节/ViewController.m
+++ 中秋节/ViewController.m
@@ -16,7 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"修改");
 }
 
 - (void)didReceiveMemoryWarning {
